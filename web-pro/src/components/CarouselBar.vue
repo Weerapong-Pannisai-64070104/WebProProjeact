c51dddab0750d12bf4a7bd5923295528662a24ed
นายชยณัฐ ศุทธยาลัย 64070136 <chayanut.oakley@gmail.com>
2023-03-01 02:13:31 +0700

    test2

--- web-pro/src/components/CarouselBar.vue
+++ web-pro/src/components/CarouselBar.vue
@@ -1,41 +1,36 @@
 <template>
-  <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-indicators m-3">
-      <button type="button" style="padding:3px;" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
-        class="active btn btn-light ml-2" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" style="padding:3px;" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-        class="btn btn-light ml-2" aria-label="Slide 2"></button>
-      <button type="button" style="padding:3px;" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-        class="btn btn-light ml-2" aria-label="Slide 3"></button>
-    </div>
-    <div class="carousel-inner">
-      <div class="carousel-item active" style="max-height: 500px;" data-bs-interval="10000">
-        <img src="https://www.phoenixnext.com/pub/media/banner/tmp/_LN_Tokidoki_Russia_Vol.4.5_EC_4__1.jpg" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-        </div>
-      </div>
-      <div class="carousel-item" style="max-height: 500px;" data-bs-interval="5000">
-        <img src="https://www.phoenixnext.com/pub/media/brand/tmp/_LN_Spy_KVEC-min_1_.jpg" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-        </div>
-      </div>
-      <div class="carousel-item" style="max-height: 500px;" data-bs-interval="9000">
-        <img src="https://www.phoenixnext.com/pub/media/banner/tmp/_LN_Kanojo_NTR_Vol.2_EC.jpg" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-        </div>
-      </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-      data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-      data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
+  <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 3"></button>
   </div>
+  <div class="carousel-inner" style="max-height: 500px">
+    <div class="carousel-item active" >
+        <img src="https://cdn.discordapp.com/attachments/959359393776959553/1080179282082537513/9786161850883L.png" class="d-block w-100 p-5 "  alt="...">
+  
+    </div>
+    <div class="carousel-item" >
+      <img src="https://cdn.discordapp.com/attachments/959359393776959553/1080179282082537513/9786161850883L.png" class="d-block w-100 p-5  " alt="...">
+    
+    </div>
+    <div class="carousel-item" >
+      <img src="https://cdn.discordapp.com/attachments/959359393776959553/1080179282082537513/9786161850883L.png" class="d-block w-100 p-5 " alt="...">
+    </div>
+    <div class="carousel-item" >
+      <img src="https://cdn.discordapp.com/attachments/959359393776959553/1080179282082537513/9786161850883L.png" class="d-block w-100 p-5 " alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 </template>
 <script>
 
@@ -43,3 +38,9 @@ export default {
 name: 'EventBar',
 }
 </script>
+<style>
+
+.carousel-inner > .item {
+   height: 400px;
+}
+</style>
--- web-pro/src/components/book.js
+++ web-pro/src/components/book.js
@@ -1,32 +1,4 @@
-
-<template>
-    <div class="bg-white">
-        <div class="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-            <h2 class="sr-only">Products</h2>
-
-            <div class="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-                <router-link v-for="product in products" :key="product.id" :to="product.href" class="group">
-                    <div
-                        class="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-                        <img :src="product.imageSrc" :alt="product.imageAlt"
-                            class="h-full w-full object-cover object-center group-hover:opacity-80" />
-                    </div>
-                    <h3 class=" mx-4 text-gray-700 text-xl ">{{ product.name }}</h3>
-                    <p class="mt-1 text-lg font-medium text-gray-900">
-                        {{ product.price }}
-
-                    </p>
-                    <button class=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                        Borrow
-                    </button>
-                </router-link>
-            </div>
-        </div>
-    </div>
-</template>
-  
-<script setup>
-const products = [
+const products =JSON.parse(`[
     {
         id: 1,
         name: "มุมมองนักอ่านพระเจ้า เล่ม 7",
@@ -34,7 +6,7 @@ const products = [
         imageSrc:
             "https://cdn.discordapp.com/attachments/959359393776959553/1080179282082537513/9786161850883L.png",
         imageAlt:
-            "มุมมองนักอ่านพระเจ้า เล่ม 7",
+            "omniscient reader's viewpoint",
     },
     {
         id: 2,
@@ -107,5 +79,4 @@ const products = [
             "Hand holding black machined steel mechanical pencil with brass tip and top.",
     },
     // More products...
-];
-</script>
+];`);
