efe3fab62c5c0445b1f99b33d6098d0c09c615b9
นายชยณัฐ ศุทธยาลัย 64070136 <chayanut.oakley@gmail.com>
2023-03-03 00:11:25 +0700

    nav

--- web-pro/src/components/NavBar.vue
+++ web-pro/src/components/NavBar.vue
@@ -1,142 +1,151 @@
 <template>
   <div>
-  <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
-    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-      <div class="relative flex h-16 items-center justify-between">
-        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-          <!-- Mobile menu button-->
-          <DisclosureButton
-            class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-            <span class="sr-only">Open main menu</span>
-            <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
-            <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
-          </DisclosureButton>
+    <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
+      <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div class="relative flex h-16 items-center justify-between">
+          <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <!-- Mobile menu button-->
+            <DisclosureButton
+              class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+              <span class="sr-only">Open main menu</span>
+              <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+              <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
+            </DisclosureButton>
 
-          <TransitionRoot as="template" :show="Open">
-            <Dialog as="div" class="relative z-10" @close="Open = false">
-              <TransitionChild as="template" enter="ease-in-out duration-500" enter-from="opacity-0"
-                enter-to="opacity-100" leave="ease-in-out duration-500" leave-from="opacity-100" leave-to="opacity-0">
-                <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-              </TransitionChild>
+            <TransitionRoot as="template" :show="Open">
+              <Dialog as="div" class="relative z-10" @close="Open = false">
+                <TransitionChild as="template" enter="ease-in-out duration-500" enter-from="opacity-0"
+                  enter-to="opacity-100" leave="ease-in-out duration-500" leave-from="opacity-100" leave-to="opacity-0">
+                  <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                </TransitionChild>
 
-              <div class="fixed inset-0 overflow-hidden">
-                <div class="absolute inset-0 overflow-hidden">
-                  <div class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
-                    <TransitionChild as="template" enter="transform transition ease-in-out duration-500 sm:duration-700"
-                      enter-from="-translate-y-full" enter-to="translate-y-0"
-                      leave="transform transition ease-in-out duration-500 sm:duration-700" leave-from="translate-y-0"
-                      leave-to="translate-y-full">
-                      <DialogPanel class="pointer-events-auto relative w-screen max-w-md">
-                        <TransitionChild as="template" enter="ease-in-out duration-500" enter-from="opacity-0"
-                          enter-to="opacity-full" leave="ease-in-out duration-500" leave-from="opacity-full"
-                          leave-to="opacity-0">
-                          <div class="bg-white rounded-lg shadow overflow-hidden">
-                            <table class="w-full">
-                              <thead class="border-b">
-                                <tr>
-                                  <th> </th>
-                                  <th class="px-4 py-2 text-left">Title</th>
-                                  <th class="px-4 py-2 text-left">Quantity</th>
-                                </tr>
-                              </thead>
-                              <tbody>
-                                <tr v-for="item in cart" :key="item.id">
-                                  <td class="px-4 py-2">
-                                    <img class="object-contain h-48 w-96" :src=item.imageSrc alt="Placeholder image">
-                                  </td>
-                                  <td class="px-4 py-2">{{ item.name }}</td>
-                                  <td class="px-4 py-2">{{ item.quantity }}</td>
-                                </tr>
-                              </tbody>
-                            </table>
-                            <span class="icon mr-2" key="false" @click="clearCart()">
-                              Clear
-                            </span>
-                            <div
-                              class=" m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full text-center"
-                              @click="goCheckout()">
-                              CheckOut
+                <div class="fixed inset-0 overflow-hidden">
+                  <div class="absolute inset-0 overflow-hidden">
+                    <div class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                      <TransitionChild as="template" enter="transform transition ease-in-out duration-500 sm:duration-700"
+                        enter-from="-translate-y-full" enter-to="translate-y-0"
+                        leave="transform transition ease-in-out duration-500 sm:duration-700" leave-from="translate-y-0"
+                        leave-to="translate-y-full">
+                        <DialogPanel class="pointer-events-auto h-75 relative w-screen max-w-md">
+                          <TransitionChild as="template" enter="ease-in-out duration-500" enter-from="opacity-0"
+                            enter-to="opacity-full" leave="ease-in-out duration-500" leave-from="opacity-full"
+                            leave-to="opacity-0">
+                            <div class="bg-white rounded-lg h-100 shadow overflow-scroll ">
+                              <table class="w-full">
+                                <thead class="border-b">
+                                  <tr>
+                                    <th>
+                                      <span class="icon mx-10 my-1 btn btn-dark" key="false" @click="clearCart()">
+                                        Clear
+                                      </span>
+                                    </th>
+                                    <th class="px-4 py-2 text-left">Title</th>
+                                    <th class="px-4 py-2 text-left">
+                                      Quantity
+                                    </th>
+                                  </tr>
+                                </thead>
+
+                                <tbody>
+                                  <tr v-for="item in cart" :key="item.id">
+                                    <td class="px-4 py-2">
+                                      <img class="object-contain h-48 w-96" :src="item.imageSrc"
+                                        alt="Placeholder image" />
+                                    </td>
+                                    <td class="px-4 py-2">{{ item.name }}</td>
+                                    <td class="px-4 py-2">
+                                      {{ item.quantity }}
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+
+                              <div
+                                class="bottom-0 inset-x-0  absolute m-1 bg-gray-500 hover:bg-black text-white font-bold py-2 px-4 rounded-full text-center"
+                                @click="goCheckout()">
+                                CheckOut
+                              </div>
+
                             </div>
-                          </div>
-                        </TransitionChild>
-                      </DialogPanel>
-                    </TransitionChild>
+                          </TransitionChild>
+                        </DialogPanel>
+                      </TransitionChild>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </Dialog>
-          </TransitionRoot>
-        </div>
-        <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-          <!-- <div class="flex flex-shrink-0 items-center">
+              </Dialog>
+            </TransitionRoot>
+          </div>
+          <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <!-- <div class="flex flex-shrink-0 items-center">
             <img class="block h-8 w-auto lg:hidden" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
             <img class="hidden h-8 w-auto lg:block" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
           </div> -->
 
-          <div class="hidden sm:ml-6 sm:block">
-            <div class="flex space-x-4">
-              <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[
-                item.current
-                  ? 'bg-gray-900 text-white'
-                  : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                'rounded-md px-3 py-2 text-sm font-medium',
-              ]" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+            <div class="hidden sm:ml-6 sm:block">
+              <div class="flex space-x-4">
+                <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[
+                  item.current
+                    ? 'bg-gray-900 text-white'
+                    : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                  'rounded-md px-3 py-2 text-sm font-medium',
+                ]" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-          <Button @click="Open = true"
-            class="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-gray-800 focus:ring-offset-2 focus:ring-offset-gray-800">
-            <span class="sr-only">Open user menu</span>
-            <ShoppingBagIcon class="h-6 w-8 rounded-full text-white" src="" alt="" />
-          </Button>
+          <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <Button @click="Open = true"
+              class="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-gray-800 focus:ring-offset-2 focus:ring-offset-gray-800">
+              <span class="sr-only">Open user menu</span>
+              <ShoppingBagIcon class="h-6 w-8 rounded-full text-white" src="" alt="" />
+            </Button>
 
-          <!-- Profile dropdown -->
-          <Menu as="div" class="relative ml-3">
-            <div>
-              <MenuButton
-                class="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 ml-5 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                <span class="sr-only">Open user menu</span>
-                <UserIcon class="h-8 w-8 rounded-full text-white" src="" alt="" />
-              </MenuButton>
-            </div>
-            <transition enter-active-class="transition ease-out duration-100"
-              enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100"
-              leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100"
-              leave-to-class="transform opacity-0 scale-95">
-              <MenuItems
-                class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                <MenuItem v-slot="{ active }">
-                <a href="/SignIn" :class="[
-                  active ? 'bg-gray-100' : '',
-                  'block px-4 py-2 text-sm text-gray-700',
-                ]">Sign In</a>
-                </MenuItem>
-                <MenuItem v-slot="{ active }">
-                <a href="/SignUp" :class="[
-                  active ? 'bg-gray-100' : '',
-                  'block px-4 py-2 text-sm text-gray-700',
-                ]">Sign Up</a>
-                </MenuItem>
-              </MenuItems>
-            </transition>
-          </Menu>
+            <!-- Profile dropdown -->
+            <Menu as="div" class="relative ml-3">
+              <div>
+                <MenuButton
+                  class="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 ml-5 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                  <span class="sr-only">Open user menu</span>
+                  <UserIcon class="h-8 w-8 rounded-full text-white" src="" alt="" />
+                </MenuButton>
+              </div>
+              <transition enter-active-class="transition ease-out duration-100"
+                enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100"
+                leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100"
+                leave-to-class="transform opacity-0 scale-95">
+                <MenuItems
+                  class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <MenuItem v-slot="{ active }">
+                  <a href="/SignIn" :class="[
+                    active ? 'bg-gray-100' : '',
+                    'block px-4 py-2 text-sm text-gray-700',
+                  ]">Sign In</a>
+                  </MenuItem>
+                  <MenuItem v-slot="{ active }">
+                  <a href="/SignUp" :class="[
+                    active ? 'bg-gray-100' : '',
+                    'block px-4 py-2 text-sm text-gray-700',
+                  ]">Sign Up</a>
+                  </MenuItem>
+                </MenuItems>
+              </transition>
+            </Menu>
+          </div>
         </div>
       </div>
-    </div>
 
-    <DisclosurePanel class="sm:hidden">
-      <div class="space-y-1 px-2 pt-2 pb-3">
-        <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[
-          item.current
-            ? 'bg-gray-900 text-white'
-            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-          'block rounded-md px-3 py-2 text-base font-medium',
-        ]" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
-      </div>
-    </DisclosurePanel>
-  </Disclosure>
-</div>
+      <DisclosurePanel class="sm:hidden">
+        <div class="space-y-1 px-2 pt-2 pb-3">
+          <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[
+            item.current
+              ? 'bg-gray-900 text-white'
+              : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+            'block rounded-md px-3 py-2 text-base font-medium',
+          ]" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+        </div>
+      </DisclosurePanel>
+    </Disclosure>
+  </div>
 </template>
 
 <script setup>
@@ -181,10 +190,10 @@ export default {
       this.active = !this.active;
     },
     goCheckout() {
-      localStorage.setItem("cart", JSON.stringify(this.cart))
+      localStorage.setItem("cart", JSON.stringify(this.cart));
       window.location.href = "/CheckOut";
     },
-  }
+  },
 };
 </script>
 
