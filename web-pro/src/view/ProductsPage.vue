a046bc5a61a2d688c0eaa5aa07bebf656babebc0
นายชยณัฐ ศุทธยาลัย 64070136 <chayanut.oakley@gmail.com>
2023-04-11 20:21:47 +0700

    update

--- web-pro/src/view/ProductsPage.vue
+++ web-pro/src/view/ProductsPage.vue
@@ -1,34 +1,133 @@
+
 <template>
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <div class="d">
-        <NavBar />
-         <ProductOver/>
+    <NavBar />
+    <div class="bg-white">
+        <div class="pt-6">
+            <nav aria-label="Breadcrumb">
+                <ol role="list" class="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+                    <li v-for="breadcrumb in product.breadcrumbs" :key="breadcrumb.id">
+                        <div class="flex items-center">
+                            <a :href="breadcrumb.href" class="mr-2 text-sm font-medium text-gray-900">{{ breadcrumb.name
+                            }}</a>
+                            <svg width="16" height="20" viewBox="0 0 16 20" fill="currentColor" aria-hidden="true"
+                                class="h-5 w-4 text-gray-300">
+                                <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
+                            </svg>
+                        </div>
+                    </li>
+                    <li class="text-sm">
+                        <a :href="product.href" aria-current="page" class="font-medium text-gray-500 hover:text-gray-600">{{
+                            product.name }}</a>
+                    </li>
+                </ol>
+            </nav>
+
+            <!-- Image gallery -->
+            <div class="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
+                <div class="aspect-w-3 aspect-h-4 hidden overflow-hidden rounded-lg lg:block">
+                    <img :src="product.images[0].src" :alt="product.images[0].alt"
+                        class="h-full w-full object-cover object-center" />
+                </div>
+                
+            </div>
+
+            <!-- Product info -->
+            <div
+                class="mx-auto max-w-2xl px-4 pt-10 pb-16 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pt-16 lg:pb-24">
+                <div class="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+                    <h1 class="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">{{ product.name }}</h1>
+                </div>
+
+                <!-- Options -->
+                <div class="mt-4 lg:row-span-3 lg:mt-0">
+                    <h2 class="sr-only">Product information</h2>
+                    <p class="text-3xl tracking-tight text-gray-900">{{ product.price }}</p>
+
+                    <!-- Reviews -->
+                    <div class="mt-6">
+                        <h3 class="sr-only">Reviews</h3>
+                        <div class="flex items-center">
+                            <div class="flex items-center">
+                                <StarIcon v-for="rating in [0, 1, 2, 3, 4]" :key="rating"
+                                    :class="[reviews.average > rating ? 'text-gray-900' : 'text-gray-200', 'h-5 w-5 flex-shrink-0']"
+                                    aria-hidden="true" />
+                            </div>
+                            <p class="sr-only">{{ reviews.average }} out of 5 stars</p>
+                            <a :href="reviews.href"
+                                class="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">{{ reviews.totalCount
+                                }} reviews</a>
+                        </div>
+                    </div>
+
+
+                </div>
+
+                <div class="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pt-6 lg:pb-16 lg:pr-8">
+                    <!-- Description and details -->
+                    <div>
+                        <h3 class="sr-only">Description</h3>
+
+                        <div class="space-y-6">
+                            <p class="text-base text-gray-900">{{ product.description }}</p>
+                        </div>
+                    </div>
+
+                    <div class="mt-10">
+                        <h3 class="text-sm font-medium text-gray-900">Highlights</h3>
+
+                        <div class="mt-4">
+                            <ul role="list" class="list-disc space-y-2 pl-4 text-sm">
+                                <li v-for="highlight in product.highlights" :key="highlight" class="text-gray-400">
+                                    <span class="text-gray-600">{{ highlight }}</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="mt-10">
+                        <h2 class="text-sm font-medium text-gray-900">Details</h2>
+
+                        <div class="mt-4 space-y-6">
+                            <p class="text-sm text-gray-600">{{ product.details }}</p>
+                        </div>
+                        
+                    </div>
+                    <br>
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                        Borrow
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
     
-    
 </template>
-
-<script>
+  
+<script setup>
 import NavBar from '../components/NavBar.vue'
-import ProductOver from '../components/ProductOver.vue'
+import { StarIcon } from '@heroicons/vue/20/solid'
 
+const product = {
+    name: 'Basic Tee 6-Pack',
+    price: '$192',
+    images: [
+        {
+            src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg',
+            alt: 'Two each of gray, white, and black shirts laying flat.',
+        }
+    ],
 
-export default {
-    name: 'ProfileUser',
-    components: {
-        NavBar,
-        ProductOver
-    }
+    description:
+        'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
+    highlights: [
+        'Hand cut and sewn locally',
+        'Dyed with our proprietary colors',
+        'Pre-washed & pre-shrunk',
+        'Ultra-soft 100% cotton',
+    ],
+    details:
+        'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
 }
+const reviews = { href: '#', average: 4, totalCount: 117 }
+
 </script>
-
-<style>
-#prou {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
-</style>
